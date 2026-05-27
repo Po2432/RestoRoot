a1df12dfc3df0185c9311396d6f99bdd8cfeb69b
Po2432 <202832399+Po2432@users.noreply.github.com>
2026-05-27 17:19:25 +0300

    Update core.php

--- src/latest/includes/core.php
+++ src/latest/includes/core.php
@@ -128,7 +128,7 @@ if (!file_exists($eulaFlag) && !in_array($currentPage, $bypassPages)) {
             <pre><?= htmlspecialchars($eulaText) ?></pre>
             <div class="instructions">
                 <strong>🔒 Access Blocked</strong><br>
-                If you are the administrator of this site and missed the agreement phase during installation, you must manually create an empty file named <code>eula.flag</code> in your root directory via FTP or File Manager to unlock your public views.\n (c) 2026 Po2432
+                If you are the administrator of this site and missed the agreement phase during installation, you must manually create an empty file named <code>eula.flag</code> in your root directory via FTP or File Manager to unlock your public views. (c) 2026 Po2432
             </div>
         </div>
     </body>
